7d2311674561fd33f916e1f7eed7a88c0c705ee6
Shubamakabra <magnus.engoe@hotmail.no>
2022-11-02 14:48:23 +0100

    update: branchign and doing stuff

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,7 @@ package cmd
 import "fmt"
 
 func main() {
-	fmt.Println("Hey im here for the first time.")
+	fmt.Println("Hey im here for the 1st time.")
+	fmt.Println("Hey im here for the 2nd time.")
+	fmt.Println("I made a branch")
 }
