6d915eb0e5af533cbd39114d878cc920f543041f
Shubamakabra <magnus.engoe@hotmail.no>
2022-11-02 15:06:11 +0100

    update: Start of the adventure

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,5 @@ func main() {
 	fmt.Println("Hey im here for the 1st time.")
 	fmt.Println("Hey im here for the 2nd time.")
 	fmt.Println("I made a branch")
-	fmt.Println("I merged a branch!")
 	fmt.Println("1")
 }
